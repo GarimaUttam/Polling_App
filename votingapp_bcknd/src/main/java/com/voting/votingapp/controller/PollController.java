685771682f8fd64069310c87ac3936e2be35160b
GarimaUttam <garimauttam191203@gmail.com>
2025-02-05 22:56:02 +0530

    post - creating a poll

--- votingapp_bcknd/src/main/java/com/voting/votingapp/controller/PollController.java
+++ votingapp_bcknd/src/main/java/com/voting/votingapp/controller/PollController.java
@@ -1,4 +1,26 @@
 package com.voting.votingapp.controller;
 
+import com.voting.votingapp.model.Poll;
+import com.voting.votingapp.services.PollService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.http.ResponseEntity;
+
+@RestController
+@RequestMapping("/api/polls")
 public class PollController {
+    private final PollService pollService;
+
+    public PollController(PollService pollService) {
+        this.pollService = pollService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Poll> createPoll(@RequestBody Poll poll) {
+        Poll createdPoll = pollService.createPoll(poll);
+        return ResponseEntity.ok(createdPoll); // Ensures correct JSON response
+    }
 }
--- votingapp_bcknd/src/main/java/com/voting/votingapp/model/Poll.java
+++ votingapp_bcknd/src/main/java/com/voting/votingapp/model/Poll.java
@@ -1,27 +1,38 @@
 package com.voting.votingapp.model;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Data
 @NoArgsConstructor
+//@JsonIgnoreProperties(ignoreUnknown = true)  // Ignores unknown fields
+@JsonInclude(JsonInclude.Include.NON_NULL)   // Ensures null values are included in response
 public class Poll {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonProperty("id")  // Ensure ID is included in the JSON response
     private Long id;
+
+    @JsonProperty("question")  // Ensure question is included in the JSON response
     private String question;
 
     @ElementCollection
+    @CollectionTable(name = "poll_options", joinColumns = @JoinColumn(name = "poll_id"))
+    @Column(name = "option_value")
+    @JsonProperty("options")  // Maps correctly to JSON
     private List<String> options = new ArrayList<>();
 
     @ElementCollection
+    @CollectionTable(name = "poll_votes", joinColumns = @JoinColumn(name = "poll_id"))
+    @Column(name = "vote_count")
+    @JsonProperty("votes")  // Maps correctly to JSON
     private List<Long> votes = new ArrayList<>();
-
-
 }
